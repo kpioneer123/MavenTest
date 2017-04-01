5057381aeb5d9dc23e77fc859e7db5d9b258ceed
xionghu <965705418@qq.com>
2017-04-01 13:47:09 +0800

    libtest

---
+++ mylibrary/src/main/java/com/kpioneer/mylibrary/LibTest.java
@@ -0,0 +1,15 @@
+package com.kpioneer.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by Xionghu at 2017/3/31 18:03
+ * function:
+ * version:
+ */
+
+public class LibTest {
+    public static void DemoLog(){
+        Log.d("DEMOLOG","哈哈哈");
+    }
+}
